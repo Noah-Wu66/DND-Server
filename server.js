7bbf207a5dcb03b3cb7f83eef50a4409b218eb92
Noah <152876591+Noah-Wu66@users.noreply.github.com>
2025-03-14 22:41:56 +0800

    Update server.js

--- server.js
+++ server.js
@@ -32,7 +32,8 @@ connectDB();
 app.use(cors({
   origin: process.env.CORS_ORIGIN || '*'
 }));
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 app.use(morgan('dev'));
 
 // 健康检查路由
